bc374ce3727eadcf30d03a458146002a9eda861c
Trevor M. Danielewski <149205715+Trevor-M-Danielewski@users.noreply.github.com>
2025-12-09 00:39:44 +0000

    Added The Cool Parts

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,14 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+	@Test
+	public void testMinus(){
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.subtract(1,1), 0);
+	}
+	@Test
+	public void testMultiply(){
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(2,3), 6);
+	}
 }
